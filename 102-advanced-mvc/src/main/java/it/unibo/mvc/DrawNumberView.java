00f50051460b835f8b935dbbb765746f936ded0d
Matteo Grandini <matteo.grandini@studio.unibo.it>
2024-11-26 17:10:36 +0100

    added displayError to DrawNumberView

--- 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberView.java
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/DrawNumberView.java
@@ -24,4 +24,10 @@ public interface DrawNumberView {
      * @param res the result of the last draw
      */
     void result(DrawResult res);
+
+    /**
+     * Display an error message on screen
+     * @param message the error message to be displayed
+     */
+    void displayError(String message);
 }
